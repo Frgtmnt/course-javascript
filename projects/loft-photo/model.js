923e9288e73b09894e47f5e433de9271d107b10f
Julia Tsoy <dk.jt666@gmail.com>
2024-01-22 00:57:01 +0300

    Выполено задание за 4 неделю

--- projects/loft-photo/model.js
+++ projects/loft-photo/model.js
@@ -1,7 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-import photosDB from './photos.json';
-// eslint-disable-next-line no-unused-vars
-import friendsDB from './friends.json';
+const APP_ID = 51837781;
 
 export default {
   getRandomElement(array) {
@@ -13,11 +10,88 @@ export default {
   
     return array[i];
   },
-  getNextPhoto() {
-    const friend = getRandomElement(friendsDB);
-    const id = friend.id;
-    const photo = getRandomElement(photosDB[id]);
 
-    return {friend : friend.firstName, url : photo.url};
+  async getNextPhoto() {
+    const friend = this.getRandomElement(this.friends.items);
+    const photos = await this.getFriendPhotos(friend.id)
+    const photo = this.getRandomElement(photos.item);
+    const photoSize = this.findSize(photo);
+
+    return {friend, id: photo.id, url : photoSize.url};
   },
+
+  findSize(photo) {
+    const size = photo.sizes.find(size => size.width >= 360);
+
+    return size;
+  },
+
+  login() {
+    return new Promise((resolve, reject) => {
+      VK.init({
+        apiId : APP_ID,
+      })
+
+      VK.Auth.login (data => {
+        if (data.session) {
+          resolve(response);
+        } else {
+          reject(new Error ('Не удалось авторизоваться'))
+        }
+      }, 2, 4)
+    })
+  },
+
+  callApi(method, params) {
+    params.v = '5.199';
+
+    return new Promise ((resolve, reject) => {
+      VK.api(method, params, (data) => {
+        if (data.error) {
+          reject(new Error (data.error.error_msg));
+        } else {
+          resolve(data.response);
+        }
+      })
+    })
+  },
+
+  async init() {
+    this.friends = await this.getFriends();
+  },
+
+  photoCache: {},
+
+  getFriends() {
+    const params = {
+      fields : ['photo_50', 'photo_100'],
+    };
+
+    return this.callApi('friends.get', params);
+  },
+
+  getPhotos(owner) {
+    const params = {
+      owner_id : owner,
+    };
+
+    return this.callApi ('photos.getAll', params);
+  },
+
+  async getFriendPhotos(id) {
+    const photos = this.photoCache[id];
+
+    if (photos) {
+      return photos;
+    }
+
+    photos = await this.getPhotos(id);
+
+    this.photoCache[id] = photos;
+
+    return photos;
+},
+
 };
+
+
